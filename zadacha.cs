4886281ad2ffc217a67427dfdfb9b743ef7ec4d9
Alexander SURikat <perm.alexx@gmail.com>
2024-02-17 15:56:26 +0500

    Создали массив для хранения

--- zadacha.cs
+++ zadacha.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         string[] originalArray = { "Hello", "2", "world", ":-)" };
+        
+        string[] newArray = new string[originalArray.Length];
+        int count = 0;
     }
 }
